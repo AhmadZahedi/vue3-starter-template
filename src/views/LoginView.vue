45c6b10811d61e93e4c7d1c3563bbeefb18d924d
Ahmad <ahmad.zahedi121288@gmail.com>
2023-11-22 11:20:58 +0330

    feat(LoginView): test tooltip in login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,17 +1,39 @@
 <template>
-    <header>
-        <nav>
-            <RouterLink :to="{ name: 'Dashboard' }">
-                {{ $t('Dashboard') }}
-            </RouterLink>
-            |
-            <RouterLink :to="{ name: 'Login' }">
-                {{ $t('Login') }}
-            </RouterLink>
-        </nav>
-    </header>
+<header>
+    <nav>
+        <RouterLink :to="{ name: 'Dashboard' }">
+            {{ $t('Dashboard') }}
+        </RouterLink>
+        |
+        <RouterLink :to="{ name: 'Login' }">
+            {{ $t('Login') }}
+        </RouterLink>
+    </nav>
+</header>
 
-    <main>
-        <h1>{{ $t('Login') }}</h1>
-    </main>
+<main>
+    <h1>{{ $t('Login') }}</h1>
+
+    <div style="height: 4000px; margin: 500px">
+        <VTooltip
+            trigger="click"
+            position="start"
+        >
+            <template #activator="{ on }">
+                <button
+                    v-on="on"
+                    type="button"
+                    class="btn btn-outline-warning text-black"
+                >
+                    click here
+                </button>
+            </template>
+
+            <span>{{ $t('Hello') }}</span>
+        </VTooltip>
+    </div>
+</main>
 </template>
+<script setup>
+import VTooltip from "@/components/VTooltip.vue";
+</script>
