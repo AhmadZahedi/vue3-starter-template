8e42f0fc4bd9fd93e1f90224305ade55ea1cbf8d
Ahmad <ahmad.zahedi121288@gmail.com>
2023-11-27 10:43:28 +0330

    feat(VTooltip): handle permanent display of tooltip

--- src/components/VTooltip.vue
+++ src/components/VTooltip.vue
@@ -13,6 +13,7 @@
 </template>
 
 <script>
+// Vue
 import { computed, nextTick, ref } from 'vue';
 
 // Enums
@@ -76,84 +77,112 @@ export default {
             return result
         });
 
-        const tooltip = ref();
+        const tooltip = ref(undefined);
         const tooltipClassNames = computed(() => {
-            return {
-                'tooltip show px-2 py-1 rounded-2': true,
-                [`bg-${props.theme} text-bg-${props.theme}`]: true,
-                ['tooltip-' + props.position]: true
-            };
+            return [
+                'tooltip show px-2 py-1 rounded-2',
+                `bg-${props.theme}`,
+                `text-bg-${props.theme}`,
+                `tooltip-${props.position}`
+            ];
         });
 
         const isShown = ref(false);
+
+        const gap = 2;
+
+        function stickToTop(tooltipEl, targetEl) {
+            tooltipEl.style.top = targetEl.top - tooltipEl.clientHeight - gap + window.scrollY + 'px';
+            tooltipEl.style.left = targetEl.left + (targetEl.width / 2) - (tooltipEl.clientWidth / 2) + 'px';
+        }
+
+        function stickToBottom(tooltipEl, targetEl) {
+            tooltipEl.style.top = targetEl.bottom + window.scrollY + gap + 'px';
+            tooltipEl.style.left = targetEl.left + (targetEl.width / 2) - (tooltipEl.clientWidth / 2) + 'px';
+        }
+
+        function stickToStart(tooltipEl, targetEl) {
+            tooltipEl.style.top = targetEl.top + (targetEl.height / 2) - (tooltipEl.clientHeight / 2) + window.scrollY + 'px';
+
+            if (LanguageService.isRtl()) {
+                tooltipEl.style.left = targetEl.left + targetEl.width + gap + 'px';
+            } else {
+                tooltipEl.style.left = targetEl.left - tooltipEl.clientWidth - gap + 'px';
+            }
+        }
+
+        function stickToEnd(tooltipEl, targetEl) {
+            tooltipEl.style.top = targetEl.top + (targetEl.height / 2) - (tooltipEl.clientHeight / 2) + window.scrollY + 'px';
+
+            if (LanguageService.isRtl()) {
+                tooltipEl.style.left = targetEl.left - tooltipEl.clientWidth - gap + 'px';
+            } else {
+                tooltipEl.style.left = targetEl.left + targetEl.width + gap + 'px';
+            }
+        }
+
+        function reset() {
+            tooltip.value.style.removeProperty('top');
+            tooltip.value.style.removeProperty('left');
+        }
 
         function show(event) {
             isShown.value = true;
 
             const rect = event.target.getBoundingClientRect();
 
-            const space = 2;
-
-            function stickToTop(tooltipEl, targetEl) {
-                tooltipEl.value.style.top = targetEl.top - tooltipEl.value.clientHeight - space + window.scrollY + 'px';
-                tooltipEl.value.style.left = targetEl.left + (targetEl.width / 2) - (tooltipEl.value.clientWidth / 2) + 'px';
-            }
-
-            function stickToBottom(tooltipEl, targetEl) {
-                tooltipEl.value.style.top = targetEl.bottom + window.scrollY + space + 'px';
-                tooltipEl.value.style.left = targetEl.left + (targetEl.width / 2) - (tooltipEl.value.clientWidth / 2) + 'px';
-            }
-
-            function stickToStart(tooltipEl, targetEl) {
-                tooltipEl.value.style.top = targetEl.top + (targetEl.height / 2) - (tooltipEl.value.clientHeight / 2) + window.scrollY + 'px';
-
-                if (LanguageService.isRtl()) {
-                    tooltipEl.value.style.left = targetEl.left + targetEl.width + space + 'px';
-                } else {
-                    tooltipEl.value.style.left = targetEl.left - tooltipEl.value.clientWidth - space + 'px';
-                }
-            }
-
-            function stickToEnd(tooltipEl, targetEl) {
-                tooltipEl.value.style.top = targetEl.top + (targetEl.height / 2) - (tooltipEl.value.clientHeight / 2) + window.scrollY + 'px';
-
-                if (LanguageService.isRtl()) {
-                    tooltipEl.value.style.left = targetEl.left - tooltipEl.value.clientWidth - space + 'px';
-                } else {
-                    tooltipEl.value.style.left = targetEl.left + targetEl.width + space + 'px';
-                }
-            }
-
-            function reset() {
-                tooltip.value.style.removeProperty('top');
-                tooltip.value.style.removeProperty('left');
-            }
-
             nextTick(() => {
                 switch (props.position) {
                     case ComponentPosition.TOP:
-                        stickToTop(tooltip, rect);
+                        stickToTop(tooltip.value, rect);
                         break;
 
                     case ComponentPosition.BOTTOM:
-                        stickToBottom(tooltip, rect);
+                        stickToBottom(tooltip.value, rect);
                         break;
 
                     case ComponentPosition.START:
-                        stickToStart(tooltip, rect);
+                        stickToStart(tooltip.value, rect);
                         break;
 
                     case ComponentPosition.END:
-                        stickToEnd(tooltip, rect);
+                        stickToEnd(tooltip.value, rect);
                         break;
                 }
 
-                console.log('top', tooltip.value.style.top)
-                console.log('left', tooltip.value.style.left.indexOf('-') > -1)
+                const tooltipRect = tooltip.value.getBoundingClientRect();
 
-                if (tooltip.value.style.left.indexOf('-') > -1) {
+                function getRectDistanceFrom(bound) {
+                    switch (bound) {
+                        case 'top':
+                            return tooltipRect.top;
+                        case 'left':
+                            return tooltipRect.left;
+                        case 'bottom':
+                            return window.innerHeight - tooltipRect.bottom;
+                        case 'right':
+                            return window.innerWidth - tooltipRect.right;
+                    }
+                }
+
+                if (getRectDistanceFrom('left') < 0) {
                     reset();
-                    stickToEnd(tooltip, rect);
+                    LanguageService.isRtl() ? stickToStart(tooltip.value, rect) : stickToEnd(tooltip.value, rect);
+                }
+
+                if (getRectDistanceFrom('right') < 0) {
+                    reset();
+                    LanguageService.isRtl() ? stickToEnd(tooltip.value, rect) : stickToStart(tooltip.value, rect);
+                }
+
+                if (getRectDistanceFrom('top') < 0) {
+                    reset();
+                    stickToBottom(tooltip.value, rect);
+                }
+
+                if (getRectDistanceFrom('bottom') < 0) {
+                    reset();
+                    stickToTop(tooltip.value, rect);
                 }
             });
         }
